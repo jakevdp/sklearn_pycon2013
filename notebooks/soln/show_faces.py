defae7530afd55f1588daee3143cef49788c4b9d
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-03-13 10:20:30 -0700

    add preliminary info; fix typos

---
+++ notebooks/soln/show_faces.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import fetch_olivetti_faces
+import numpy as np
+import matplotlib.pyplot as plt
+
+faces = fetch_olivetti_faces()
+
+# set up the figure
+fig = plt.figure(figsize=(6, 6))  # figure size in inches
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+# plot the faces: each image is 64x64 pixels
+for i in range(64):
+    ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])
+    ax.imshow(faces.images[i], cmap=plt.cm.bone)
