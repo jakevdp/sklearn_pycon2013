2a456e75eaae23263a757e34af511e8a45a2760f
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-03-12 17:02:34 -0700

    remove old solutions

--- notebooks/soln/01-02.py
+++
@@ -1,6 +0,0 @@
-# 01-02.py
-num0 = i0.sum()
-num1 = i1.sum()
-
-prior0 = num0 / float(Ntrain)
-prior1 = num1 / float(Ntrain)
--- notebooks/soln/03-01.py
+++
@@ -1,9 +0,0 @@
-#03-01.py
-X, y = preprocess(data, shuffle=False, n_samples=1000, normalization=None)
-
-from sklearn.manifold import LocallyLinearEmbedding
-lle = LocallyLinearEmbedding(n_neighbors=15,
-                             n_components=3, method='standard')
-X_proj = lle.fit_transform(X)
-
-three_component_plot(X_proj[:, 0], X_proj[:, 1], X_proj[:, 2], y, labels, trim_outliers=True)
